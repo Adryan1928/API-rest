9ba02a52699dc2e41e3aa46c5c5887d51d5ad324
Adryan1928 <adryanoliveira1928@gmail.com>
2023-08-09 15:22:19 -0300

    criei a url, falta o formulário de adicionar cliente

--- api/urls.py
+++ api/urls.py
@@ -9,5 +9,6 @@ router.register(r'clientes', ClienteViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/', include(router.urls))
+    path('api/', include(router.urls)),
+    path('clientes/', include('cliente.urls'))
 ]
